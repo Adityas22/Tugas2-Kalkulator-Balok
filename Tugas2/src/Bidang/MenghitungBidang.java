29785d6fa15657af09a53f75cb68428aa6662f5b
Adityas22 <127333783+Adityas22@users.noreply.github.com>
2023-03-22 00:33:30 +0700

    Add files via upload

---
+++ Tugas2/src/Bidang/MenghitungBidang.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Bidang;
+
+/**
+ *
+ * @author dell
+ */
+public interface MenghitungBidang {
+    double luas();
+    double keliling();
+}
